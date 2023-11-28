0ea287ae1f3b61db5c519cb3faa616b8489ab0a4
Pooja <gujalwarpooja@gmail.com>
2023-11-28 14:50:45 +0100

    Selenium Framework and Practice

--- src/main/java/pom/eurotech/test/day03_WebElementIntro/PracticeFacebook.java
+++ src/main/java/pom/eurotech/test/day03_WebElementIntro/PracticeFacebook.java
@@ -9,16 +9,17 @@ public class PracticeFacebook {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.get("https://www.facebook.com/login");
+        WebElement acceptCookies = driver.findElement(By.cssSelector("button[title = 'Allow all cookies']"));
+        acceptCookies.click();
+        Thread.sleep(2000);
         WebElement inputEmail = driver.findElement(By.id("email"));
         inputEmail.sendKeys("poojagujalwar@rediffmail.com");
         WebElement inputPassword = driver.findElement(By.id("pass"));
         inputPassword.sendKeys("9421302556");
         Thread.sleep(1000);
-        driver.findElement(By.className("42ft")).click();
 
-        Thread.sleep(1000);
         driver.findElement(By.id("loginbutton")).click();
-        Thread.sleep(1000);
+        Thread.sleep(3000);
 
         driver.close();
 
